c866829255015996adec321cacb26306528bc576
Quan Ta <quantq.dev@gmail.com>
2024-05-23 09:29:43 +0700

    Add docker

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as docker from "@pulumi/docker";
+
+// Find the latest nginx image.
+const nginxRemoteImage = new docker.RemoteImage("nginxRemoteImage", {name: "nginx"});
+// Start a container
+const nginxContainer = new docker.Container("nginxContainer", {image: nginxRemoteImage.imageId});
