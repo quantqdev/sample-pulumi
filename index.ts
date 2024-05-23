51ea2530008d92ecd493842c9d448c5890061109
Quan Ta <quantq.dev@gmail.com>
2024-05-23 11:24:17 +0700

    Add secret

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as docker from "@pulumi/docker";
 
+const config = new pulumi.Config();
+const nginxContainerName = config.require("nginxContainerName");
+// pulumi replaces secret in input of console.log
+// this will print `[secret]Name: [secret]`
+// this can be a trick to guess the secret
+console.log(`nginxContainerName: ${nginxContainerName}`); 
+
 // Find the latest nginx image.
 const nginxRemoteImage = new docker.RemoteImage("nginxRemoteImage", {name: "nginx"});
 // Start a container
-const nginxContainer = new docker.Container("nginxContainer", {image: nginxRemoteImage.imageId});
+const nginxContainer = new docker.Container(nginxContainerName, {image: nginxRemoteImage.imageId});
